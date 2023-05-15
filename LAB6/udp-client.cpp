2e3db0cfb7e7c56ee9319d397d0d93635860476d
OlesiaPalchunova <114547745+OlesiaPalchunova@users.noreply.github.com>
2023-05-15 14:31:01 +0700

    Update udp-client.cpp

--- LAB6/udp-client.cpp
+++ LAB6/udp-client.cpp
@@ -27,25 +27,28 @@ int main() {
     std::cout << "Enter message: ";
     std::cin.getline(buffer, MAX_BUFFER_SIZE); // считываем некоторое сообщение от клиента
 
-    // отправляем это сообщение
-    int sent_bytes = sendto(client_socket, buffer, std::strlen(buffer), 0, (struct sockaddr*)&server_address, sizeof(server_address));
-    if (sent_bytes < 0) {
-        std::cerr << "Failed to send data\n";
-        return 1;
-    }
+    while (true){
+        // отправляем это сообщение
+        int sent_bytes = sendto(client_socket, buffer, std::strlen(buffer), 0, (struct sockaddr*)&server_address, sizeof(server_address));
+        if (sent_bytes < 0) {
+            std::cerr << "Failed to send data\n";
+            return 1;
+        }
 
-    // получаем новые данные от сервера
-    int server_address_length = sizeof(server_address);
-    std::memset(buffer, 0, MAX_BUFFER_SIZE);
-    int received_bytes = recvfrom(client_socket, buffer, MAX_BUFFER_SIZE, 0, (struct sockaddr*)&server_address, (socklen_t*)&server_address_length);
-    if (received_bytes < 0) {
-        std::cerr << "Failed to receive data\n";
-        return 1;
-    }
+        // получаем новые данные от сервера
+        int server_address_length = sizeof(server_address);
+        std::memset(buffer, 0, MAX_BUFFER_SIZE);
+        int received_bytes = recvfrom(client_socket, buffer, MAX_BUFFER_SIZE, 0, (struct sockaddr*)&server_address, (socklen_t*)&server_address_length);
+        if (received_bytes < 0) {
+            std::cerr << "Failed to receive data\n";
+            return 1;
+        }
 
-    // выводим их на экран
-    std::cout << "Received " << received_bytes << " bytes from " << inet_ntoa(server_address.sin_addr) << ":" << ntohs(server_address.sin_port) << "\n";
-    std::cout << "Data: " << buffer << "\n";
+        // выводим их на экран
+        std::cout << "Received " << received_bytes << " bytes from " << inet_ntoa(server_address.sin_addr) << ":" << ntohs(server_address.sin_port) << "\n";
+        std::cout << "Data: " << buffer << "\n";
+        sleep(1);
+    }   
 
     close(client_socket);
     return 0;
